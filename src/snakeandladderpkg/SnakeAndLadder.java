5bfb7ec5599e46e0ab95cdddaeed383d0f68429e
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-09 21:15:07 +0530

    Refactered: Added Die Roll count and its position.

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -3,12 +3,12 @@ package snakeandladderpkg;
 public class SnakeAndLadder {
 	public static void main (String[] args) {
 		int playerPosition = 0;
-		int dice = 0, checkOption;
+		int dice = 0, checkOption, count = 0;
 		String player1 = "Player1";
 		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
 		System.out.println(player1+" at start position "+playerPosition);
 		
-		while (playerPosition >= 0 && playerPosition < 100) {
+		while (playerPosition >= 0 && playerPosition < 10) {
 			dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			System.out.println("\ndice "+dice);
 		
@@ -16,16 +16,16 @@ public class SnakeAndLadder {
 			
 			switch(checkOption) {
 			case 1:
-				System.out.println("\nYeah! You got a Ladder. ");
+				System.out.println("Yeah! You got a Ladder. ");
 				playerPosition += dice;
-				if(playerPosition > 100) {
+				if(playerPosition > 10) {
 					playerPosition -= dice;					
 				}
 				System.out.println("Your position is : " +playerPosition);
 				break;
 				
 			case 2:
-				System.out.println("\nOh No! Snake bit you. ");
+				System.out.println("Oh No! Snake bit you. ");
 				playerPosition -= dice;
 				if(playerPosition < 0) {
 					playerPosition = 0;
@@ -34,9 +34,12 @@ public class SnakeAndLadder {
 				break;
 	
 			case 3:
-				System.out.println("\nPass the chance. ");
+				System.out.println("Pass the chance. ");
 				System.out.println("Your position is : " +playerPosition);
 			}
+			count++;
+			System.out.println("Die Roll count="+count+" Position="+playerPosition);
+			
 		}
 		
 		if(playerPosition <= 0) {
@@ -44,7 +47,7 @@ public class SnakeAndLadder {
 			System.out.println("\nYour Position is :"+playerPosition);
 		}
 		
-		if(playerPosition == 100){
+		if(playerPosition == 10){
 			System.out.println("\nCongratulations!!! You Won!");
 		}
 		
