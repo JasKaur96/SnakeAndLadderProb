45381aec08d9b2a33e396094d4314f72fa00ae5f
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-09 15:30:25 +0530

    Added: Rolled the Dice.

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -2,10 +2,15 @@ package snakeandladderpkg;
 
 public class SnakeAndLadder {
 	public static void main (String[] args) {
-		int start = 0;
+		int playerPosition = 0, dice;
 		String player1 = "Player1";
 		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
-		System.out.println(player1+" at start position "+start);
-		
+		System.out.println(player1+" at start position "+playerPosition);
+
+		dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		playerPosition += dice; 
+		System.out.println("Dice shows : "+dice);
+		System.out.println(player1+" is at position "+playerPosition);
+
 	}
 }
