845622e8c187daeed4940b5c0383f756ca4df14a
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-09 20:31:49 +0530

    Refactered: Added Winnig position 100

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -3,28 +3,44 @@ package snakeandladderpkg;
 public class SnakeAndLadder {
 	public static void main (String[] args) {
 		int playerPosition = 0;
-		int dice, checkOption;
+		int dice = 0, checkOption;
 		String player1 = "Player1";
 		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
 		System.out.println(player1+" at start position "+playerPosition);
 		
-		dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-						
-		checkOption = (int) Math.floor(Math.random() * 10) % 3 + 1;
-		switch(checkOption) {
-		case 1:
-			System.out.println("\nPass the chance. ");
-			break;
-		case 2:
-			System.out.println("\nYeah! You got a Ladder. ");
-			playerPosition += dice;
-			System.out.println("Your position is : " +playerPosition);
-			break;
-		case 3:
-			System.out.println("\nOh No! Snake bit you. ");
-			playerPosition -= dice;
-			System.out.println("Your position is : " +playerPosition);
+		while (playerPosition >= 0 && playerPosition < 10) {
+			dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+			System.out.println("\ndice "+dice);
+		
+			checkOption = (int) Math.floor(Math.random() * 10) % 3 + 1;
 			
+			switch(checkOption) {
+			case 1:
+				System.out.println("\nYeah! You got a Ladder. ");
+				playerPosition += dice;
+				System.out.println("Your position is : " +playerPosition);
+				break;
+				
+			case 2:
+				System.out.println("\nOh No! Snake bit you. ");
+				playerPosition -= dice;
+				System.out.println("Your position is : " +playerPosition);
+				break;
+	
+			case 3:
+				System.out.println("\nPass the chance. ");
+				System.out.println("Your position is : " +playerPosition);
+			}
 		}
+		
+		if(playerPosition <= 0) {
+			playerPosition = 0;
+			System.out.println("\nYour Position is :"+playerPosition);
+		}
+		
+		if(playerPosition >= 10){
+			System.out.println("\nCongratulations!!! You Won!");
+		}
+		
 	}
 }
