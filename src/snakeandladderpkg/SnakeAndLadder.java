c1105b9c6383c3ba31aa7d31ed66858f3b802acd
JasKaur96 <jaspreetflora7@gmail.com>
2021-02-09 14:17:16 +0530

    Added: Player1 Start Positon at 0.

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -2,6 +2,10 @@ package snakeandladderpkg;
 
 public class SnakeAndLadder {
 	public static void main (String[] args) {
-		System.out.println("Welcome to the Snake and Ladder Game.");
+		int start = 0;
+		String player1 = "Player1";
+		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
+		System.out.println(player1+" at start position "+start);
+		
 	}
 }
