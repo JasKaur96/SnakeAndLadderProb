371aafb190b03c610faff4f291304fa02181c0a9
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-10 21:37:22 +0530

    Update SnakeAndLadder.java

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -1,5 +1,4 @@
 package snakeandladderpkg;
-
 public class SnakeAndLadder {
 	public static void main (String[] args) {
 		int playerPosition = 0;
@@ -7,40 +6,32 @@ public class SnakeAndLadder {
 		String player1 = "Player1";
 		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
 		System.out.println(player1+" at start position "+playerPosition);
-		
 		while (playerPosition >= 0 && playerPosition < 10) {
 			dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 			System.out.println("\ndice "+dice);
-		
 			checkOption = (int) Math.floor(Math.random() * 10) % 3 + 1;
-			
 			switch(checkOption) {
 			case 1:
 				System.out.println("\nYeah! You got a Ladder. ");
 				playerPosition += dice;
 				System.out.println("Your position is : " +playerPosition);
 				break;
-				
 			case 2:
 				System.out.println("\nOh No! Snake bit you. ");
 				playerPosition -= dice;
 				System.out.println("Your position is : " +playerPosition);
 				break;
-	
 			case 3:
 				System.out.println("\nPass the chance. ");
 				System.out.println("Your position is : " +playerPosition);
 			}
 		}
-		
 		if(playerPosition <= 0) {
 			playerPosition = 0;
 			System.out.println("\nYour Position is :"+playerPosition);
 		}
-		
 		if(playerPosition >= 10){
 			System.out.println("\nCongratulations!!! You Won!");
 		}
-		
 	}
 }
