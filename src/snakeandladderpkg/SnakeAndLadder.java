5f63f264fb5811be3e1c231aa3dd4c71970be260
JasKaur96 <77604638+JasKaur96@users.noreply.github.com>
2021-02-10 21:35:56 +0530

    Update SnakeAndLadder.java

--- src/snakeandladderpkg/SnakeAndLadder.java
+++ src/snakeandladderpkg/SnakeAndLadder.java
@@ -1,16 +1,13 @@
 package snakeandladderpkg;
-
 public class SnakeAndLadder {
 	public static void main (String[] args) {
 		int playerPosition = 0, dice;
 		String player1 = "Player1";
 		System.out.println("*****Welcome to the Snake and Ladder Game.*****");
 		System.out.println(player1+" at start position "+playerPosition);
-
 		dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
 		playerPosition += dice; 
 		System.out.println("Dice shows : "+dice);
 		System.out.println(player1+" is at position "+playerPosition);
-
 	}
 }
